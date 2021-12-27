f8e0a39d9c4271e8685c5074074628a85b1d9e5f
Asad Ali <asadalipkp9@gmail.com>
2021-12-27 18:14:42 +0500

    fixed non-resizeable window size

--- src/main/java/com/ourbank/bankmanagementsystem/DashboardApplication.java
+++ src/main/java/com/ourbank/bankmanagementsystem/DashboardApplication.java
@@ -11,9 +11,10 @@ public class DashboardApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DashboardApplication.class.getResource("dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 633, 410);
         stage.setTitle("OurBank Dashboard");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
